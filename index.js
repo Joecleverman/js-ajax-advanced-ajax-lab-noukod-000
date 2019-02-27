debcf6b1f5908d4d7b00e1b4d3b04298e434860e
Josue Abraham <josuea@noukod.com>
2019-02-27 19:54:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  Handlebars.registerPartial(
+    'authorPartial',
+    document.getElementById('author-partial-template').innerHTML
+  );
+});
+
+
+//initializing a new XMLHttpRequest()
+//and making a GET request to the URI for the list user repositories API
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
+}
+
+//defining a callback function, showRepositories, to handle the response
+
+//JSON.parse to parse the response string into proper objects
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos
+    .map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
 }
 
 function showRepositories(event, data) {
@@ -12,9 +34,3 @@ function showRepositories(event, data) {
   const repoList = template(repos);
   document.getElementById('repositories').innerHTML = repoList;
 }
-document.addEventListener('DOMContentLoaded', function(event) {
-  Handlebars.registerPartial(
-    'authorPartial',
-    document.getElementById('author-partial-template').innerHTML
-  );
-});
